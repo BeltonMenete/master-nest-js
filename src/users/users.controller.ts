88af08865cae33f3c7dae148da2f2fb3eee697cf
codebelton <beltonmenete@hotmail.com>
2025-11-28 21:14:48 +0200

    import & use endpoint decorators of nest

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Patch, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 @Controller()
 export class UsersController {
-  @Get('users')
-  public getUsers() {
-    return ' You sent a get request to users endpoint with no params';
-  }
+  // @Get('users')
+  // public getUsers() {
+  //   return ' You sent a get request to users endpoint with no params';
+  // }
 
-  @Get('users/*id/*txt')
+  @Get('users{/*id}{/*txt}')
   public getUser(@Param() params?: any) {
-    return `you sent a get request to users endpoints: ${params.id} and ${params.txt}`;
+    return `you sent a get request to users endpoints: ${params.id} & ${params.txt}`;
   }
 
   @Post('users')
