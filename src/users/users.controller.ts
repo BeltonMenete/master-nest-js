b46fa5da99e308241be6cd690f872b4fde9b8ed5
codebelton <beltonmenete@hotmail.com>
2025-11-28 15:58:57 +0200

    Add learn routing in nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,29 @@
-import { Controller } from '@nestjs/common';
-
-type User = {
-  id: number;
-  name: string;
-};
+import { Controller, Get, Post, Patch, Delete, Param } from '@nestjs/common';
 
 @Controller()
-export class UsersController {}
+export class UsersController {
+  @Get('users')
+  public getUsers() {
+    return ' You sent a get request to users endpoint with no params';
+  }
+
+  @Get('users/*id/*txt')
+  public getUser(@Param() params?: any) {
+    return `you sent a get request to users endpoints: ${params.id} and ${params.txt}`;
+  }
+
+  @Post('users')
+  public createUser() {
+    return 'You sent a post request to users endpoint';
+  }
+
+  @Patch('users')
+  public updateUser() {
+    return ' You sent a patch request to this endpoint user';
+  }
+
+  @Delete('users')
+  public deleteUser(@Param('id') id?: string) {
+    return 'You sent a delete request to this endpoint user ';
+  }
+}
