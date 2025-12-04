2e5a18416d12eceb8fea4c48844674f5f64bcb41
codebelton <beltonmenete@hotmail.com>
2025-12-05 01:19:40 +0200

    add class validator

---
+++ src/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+}
